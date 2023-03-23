82737a0835fc37fcad3afa4aabaecdd5119d96cd
RIchard Dawson <r.dawson@me.com>
2023-03-23 19:40:25 +0000

    dsd

--- components/shelly_dallas/sensor.py
+++ components/shelly_dallas/sensor.py
@@ -7,14 +7,21 @@ from . import ShellyDallasComponent, shelly_dallas_ns
 
 DallasTemperatureSensor = shelly_dallas_ns.class_('ShellyDallasTemperatureSensor', sensor.Sensor)
 
-CONFIG_SCHEMA = cv.All(sensor.sensor_schema(unit_of_measurement=UNIT_CELSIUS, icon=ICON_THERMOMETER, accuracy_decimals=1).extend({
-    cv.GenerateID(): cv.declare_id(DallasTemperatureSensor),
-    cv.GenerateID(CONF_DALLAS_ID): cv.use_id(ShellyDallasComponent),
-
-    cv.Optional(CONF_ADDRESS): cv.hex_int,
-    cv.Optional(CONF_INDEX): cv.positive_int,
-    cv.Optional(CONF_RESOLUTION, default=12): cv.int_range(min=9, max=12),
-}), cv.has_exactly_one_key(CONF_ADDRESS, CONF_INDEX))
+CONFIG_SCHEMA = cv.All(
+    sensor.sensor_schema(
+        DallasTemperatureSensor,
+        unit_of_measurement=UNIT_CELSIUS,
+        accuracy_decimals=1,
+    ).extend(
+        {
+            cv.GenerateID(CONF_DALLAS_ID): cv.use_id(ShellyDallasComponent),
+            cv.Optional(CONF_ADDRESS): cv.hex_int,
+            cv.Optional(CONF_INDEX): cv.positive_int,
+            cv.Optional(CONF_RESOLUTION, default=12): cv.int_range(min=9, max=12),
+        }
+    ),
+    cv.has_exactly_one_key(CONF_ADDRESS, CONF_INDEX),
+)
 
 
 def to_code(config):
